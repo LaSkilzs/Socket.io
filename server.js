e30dc2d83e4e763e48172d419a04501cc27df97d
La Fountain <github email address>
2019-08-13 20:46:50 -0400

    Completed chat application

--- server.js
+++ server.js
@@ -18,12 +18,25 @@ io.sockets.on("connection", socket => {
   console.log("Connected: %s sockets connected", connections.length);
 
   socket.on("disconnect", data => {
+    // if (!socket.username) return;
+    users.splice(users.indexOf(socket.username), 1);
+    updateUsernames();
     connections.splice(connections.indexOf(socket), 1);
     console.log("Disconnected: %s sockets connected", connections.length);
   });
 
   socket.on("send message", data => {
-    console.log(data);
     io.sockets.emit("new message", data);
   });
+
+  socket.on("new user", (data, callback) => {
+    callback(true);
+    socket.username = data;
+    users.push(socket.username);
+    updateUsernames();
+  });
+
+  function updateUsernames() {
+    io.sockets.emit("get users", users);
+  }
 });
